ecb6bfd5223c3143d72be0a6b47609bac889321d
Chris Larsen <chris.larsen@qlik.com>
2018-09-25 09:14:22 -0400

    Add files via upload

--- test_examples.py
+++ test_examples.py
@@ -2,20 +2,24 @@ from engine_communicator import EngineCommunicator
 from engine_global_api import EngineGlobalApi
 from engine_app_api import EngineAppApi
 import time
+import json
 
 url = 'ws://localhost:4848/app'
 conn = EngineCommunicator(url)
 ega = EngineGlobalApi(conn)
 eaa = EngineAppApi(conn)
 #conn.connect()
-#print conn.get_doc_list()
+doclist=ega.get_doc_list() 
+for doc in doclist:
+	print doc['qTitle']
+	
 #print conn.get_os_name()
-ega.create_app('theApp')
-time.sleep(1)
-ega.open_doc('theApp')
+#ega.create_app('theApp')
+#time.sleep(1)
+ega.open_doc('Executive Dashboard')
 doc = ega.get_active_doc()
 time.sleep(1)
 handle = ega.get_doc_handle(doc)
 print eaa.get_script(handle)
-ega.delete_app('C:\\Users\\Niklas\\Documents\\Qlik\\Sense\\Apps\\theApp.qvf')
+#ega.delete_app('C:\\Users\\cls\\Documents\\Qlik\\Sense\\Apps\\theApp.qvf')
 conn.close_qvengine_connection(conn)
